5551fc5d45258cfee6595e9a7dafe99c69c9ecb4
Valrex5 <138341257+Valrex5@users.noreply.github.com>
2024-05-26 21:36:41 -0300

    New signatures for the elephants
    One With The Earth is not fully functional though

--- Plugins/ModTectonic Paradox Pokémon/Paradox Moves.rb
+++ Plugins/ModTectonic Paradox Pokémon/Paradox Moves.rb
@@ -44,7 +44,7 @@ end
 #===============================================================================
 class PokeBattle_Move_BoostIfSuperEffective < PokeBattle_Move
     def pbBaseDamage(baseDmg, user, target)
-        baseDmg *= 1.5 if Effectiveness.super_effective?(typeModToCheck(user.battle, type, user, target, move, aiCheck))
+        baseDmg *= 1.33 if Effectiveness.super_effective?(typeModToCheck(user.battle, type, user, target, move, aiCheck))
         return baseDmg
     end
 end
@@ -53,23 +53,36 @@ end
 # Two turn attack. Skips first turn, attacks second turn. In sunshine, takes 1 turn instead. (Solar Beam)
 #===============================================================================
 class PokeBattle_Move_TwoTurnAttackCanChooseOne < PokeBattle_TwoTurnMove
-    def pbChargingTurnMessage(user, _targets)
+
+    def resolutionChoice(user)
+        @choices = [_INTL("Charge"),_INTL("Attack")]
+        @choice = @battle.scene.pbShowCommands(_INTL("Should #{user.pbThis(true)} charge the attack?"),@choices,0)
+    end 
+
+    def pbChargingTurnMessage(user, _targets)        
         @battle.pbDisplay(_INTL("{1} charges power!",user.pbThis))
     end
 
-    def resolutionChoice(user)
-        choices = [_INTL("Attack"),_INTL("Charge")]
-        choice = @battle.scene.pbShowCommands(_INTL("Should #{user.pbThis(true)} charge the attack?"),choices,0)
-    end 
+    
+    def skipChargingTurn?(user)
+        return @choice == 1
+    end
+
+    def pbBaseDamage(baseDmg, _user, target)
+        baseDmg *= 0.5 if @choice == 1
+        return baseDmg
+    end
+end
+
 
     def resetMoveUsageState
-        choice = nil
-    end
+        @choice = nil
+    end 
 
     def getEffectScore(_user, _target)
         return 100
-    end
-end
+    end 
+
 
 #===============================================================================
 # Clears hazards, weather and rooms. (Mighty Cleave?)
@@ -109,3 +122,92 @@ class PokeBattle_Move_RemovesHazardsWeatherRooms < PokeBattle_Move
         @battle.endWeather
     end
 end
+
+#===============================================================================
+# User heals for 1/4ths of their HP and removes hazards from its field. (One With The Earth) (not functional)
+#===============================================================================
+class PokeBattle_Move_HealUserOneQuartersRemoveHazards < PokeBattle_HealingMove
+    def healRatio(_user)
+        return 1.0 / 4.0
+    end
+    def hazardRemovalMove?; return true; end
+    def aiAutoKnows?(pokemon); return false; end
+
+    def initialize(battle, move)
+        super
+        @miscEffects = %i[Mist Safeguard]
+    end
+
+    def eachDefoggable(side, isOurSide)
+        side.eachEffect(true) do |effect, _value, data|
+            if !isOurSide && (data.is_screen? || @miscEffects.include?(effect))
+                yield effect, data
+            elsif data.is_hazard?
+                yield effect, data
+            end
+        end
+    end
+
+    def pbFailsAgainstTarget?(user, target, show_message)
+        targetSide = target.pbOwnSide
+        ourSide = user.pbOwnSide
+        eachDefoggable(targetSide, false) do |_effect, _data|
+            return false
+        end
+        eachDefoggable(ourSide, true) do |_effect, _data|
+            return false
+        end
+    end
+
+    def blowAwayEffect(user, side, effect, data)
+        side.disableEffect(effect)
+        if data.is_hazard?
+            hazardName = data.name
+            @battle.pbDisplay(_INTL("{1} absorbed {2}!", user.pbThis, hazardName)) unless data.has_expire_proc?
+        end
+    end
+
+    def getEffectScore(user, target)
+        score = 0
+        # Dislike removing hazards that affect the enemy
+        score -= 0.8 * hazardWeightOnSide(target.pbOwnSide) if target.alliesInReserve?
+        # Like removing hazards that affect us
+        score += hazardWeightOnSide(target.pbOpposingSide) if user.alliesInReserve?
+        target.pbOwnSide.eachEffect(true) do |effect, value, data|
+            next unless data.is_screen? || @miscEffects.include?(effect)
+			case value
+				when 2
+					score += 30
+				when 3
+					score += 55
+				when 4..999
+					score += 140
+            end	
+        end
+        return score
+    end
+end
+
+#===============================================================================
+# Power increases by 30 for each consecutive use. (Drag Race)
+#===============================================================================
+class PokeBattle_Move_DragRace < PokeBattle_SnowballingMove
+    def initialize(battle, move)
+        @usageCountEffect = :DragRace
+        super
+    end
+
+    def damageAtCount(baseDmg, count)
+        return baseDmg + 30 * count
+    end
+end
+
+GameData::BattleEffect.register_effect(:Battler, {
+    :id => :DragRace,
+    :real_name => "Drag Race Count",
+    :type => :Integer,
+    :maximum => 4,
+    :resets_on_cancel => true,
+    :resets_on_move_start => true,
+    :snowballing_move_counter => true,
+})
