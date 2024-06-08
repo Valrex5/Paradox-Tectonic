238891c0c100723cd3dc2f186ff832f10054b648
Valrex5 <138341257+Valrex5@users.noreply.github.com>
2024-06-08 17:22:48 -0300

    Organized some code

--- Plugins/ModTectonic Paradox Pokémon/Paradox Abilities.rb
+++ Plugins/ModTectonic Paradox Pokémon/Paradox Abilities.rb
@@ -1,3 +1,83 @@
+#===============================================================================
+#+50% chance of added effects in sound moves activating (Primal Echo)
+#===============================================================================
+
+BattleHandlers::AddedEffectChanceModifierUserAbility.add(:PRIMALECHO,
+    proc { |ability, user, target, move, chance|
+        chance += 50 if move.soundMove?
+        next chance
+    }
+)
+
+#===============================================================================
+# Adds the Poison Typing (Ball Mimic)
+#===============================================================================
+
+BattleHandlers::TypeCalcAbility.add(:BALLMIMIC,
+    proc { |ability, battler, types|
+        types.push(:POISON)
+        next types
+    }
+)
+
+#===============================================================================
+# Boosts special attack 50%, but locked to one move (Banshee's Melisma)
+#===============================================================================
+
+BattleHandlers::SpecialAttackCalcUserAbility.add(:BANSHEESMELISMA,
+  proc { |ability, _user, _battle, spAtkMult|
+      spAtkMult *= 1.5
+      next spAtkMult
+  }
+)
+
+#===============================================================================
+# Uses Dragon Dance when a Total Eclipse occurs (Ancient Dance)
+#===============================================================================
+
+BattleHandlers::TotalEclipseAbility.add(:ANCIENTDANCE,
+  proc { |ability, battler, battle, aiCheck|
+      next battle.forceUseMove(battler, :DRAGONDANCE, -1, ability: ability, aiCheck: aiCheck)
+  }
+) 
+
+#===============================================================================
+# Uses Magnet Rise when loses half its HP (Strong Magnetism)
+#===============================================================================
+
+BattleHandlers::AbilityOnHPDroppedBelowHalf.add(:STRONGMAGNETISM,
+  proc { |ability, battler, battle, aiCheck|
+      next battle.forceUseMove(battler, :MAGNETRISE, -1, ability: ability, aiCheck: aiCheck)
+  }
+)
+
+#===============================================================================
+# Boosts 50% Dragon-type moves in Moonglow (Crimson Skies)
+#===============================================================================
+
+BattleHandlers::DamageCalcUserAbility.add(:CRIMSONSKIES,
+  proc { |ability, user, target, move, mults, _baseDmg, type, aiCheck|
+    if user.battle.moonGlowing? && type == :DRAGON
+      mults[:base_damage_multiplier] *= 1.5
+      user.aiLearnsAbility(ability) unless aiCheck
+    end
+  }
+)
+
+#===============================================================================
+# Leech immunity (Fighting Vigor)
+#===============================================================================
+
+BattleHandlers::StatusImmunityAbility.add(:FIGHTINGVIGOR,
+  proc { |ability, _battler, status|
+      next true if status == :LEECHED
+  }
+)
+
+#===============================================================================
+# Doubles speed while using punching moves (Rocket Hands)
+#===============================================================================
+
 BattleHandlers::AbilityOnSwitchIn.add(:ROCKETHANDS,
   proc { |ability, battler, battle, aiCheck|
       next 0 if aiCheck
@@ -25,37 +105,9 @@ BattleHandlers::SpeedCalcAbility.add(:ROCKETHANDS,
     }
 )
 
-BattleHandlers::TotalEclipseAbility.add(:ANCIENTDANCE,
-  proc { |ability, battler, battle, aiCheck|
-      next battle.forceUseMove(battler, :DRAGONDANCE, -1, ability: ability, aiCheck: aiCheck)
-  }
-)
-
-BattleHandlers::AbilityOnHPDroppedBelowHalf.add(:STRONGMAGNETISM,
-  proc { |ability, battler, battle, aiCheck|
-      next battle.forceUseMove(battler, :MAGNETRISE, -1, ability: ability, aiCheck: aiCheck)
-  }
-)
-
-BattleHandlers::UserAbilityEndOfMove.add(:THUNDERSTORM,
-  proc { |ability, user, _targets, move, battle, _switchedBattlers|
-      next if battle.futureSight
-      next unless battle.sandy?
-      next if user.effectActive?(:Thunderstorm)
-      next if user.effectActive?(:Charge)
-      battle.pbShowAbilitySplash(user, ability)
-      user.applyEffect(:Charge)
-      user.applyEffect(:Thunderstorm)
-      battle.pbHideAbilitySplash(user)
-  }
-)
-
-BattleHandlers::TypeCalcAbility.add(:BALLMIMIC,
-    proc { |ability, battler, types|
-        types.push(:POISON)
-        next types
-    }
-)
+#===============================================================================
+# Heals 1/8th of its HP and takes 35% less damage in sun (Solar Panel)
+#===============================================================================
 
 BattleHandlers::EORWeatherAbility.add(:SOLARPANEL,
     proc { |ability, _weather, battler, battle|
@@ -74,27 +126,52 @@ BattleHandlers::DamageCalcTargetAbility.add(:SOLARPANEL,
   }
 )
 
-BattleHandlers::DamageCalcUserAbility.add(:CRIMSONSKIES,
-  proc { |ability, user, target, move, mults, _baseDmg, type, aiCheck|
-    if user.battle.moonGlowing? && type == :DRAGON
-      mults[:base_damage_multiplier] *= 1.5
-      user.aiLearnsAbility(ability) unless aiCheck
-    end
+#===============================================================================
+# Charges once per switch-in in sandstorm (Thunderstorm)
+# Bug immunity + raises speed if hit by one (Impenetrable Shell)
+#===============================================================================
+
+BattleHandlers::UserAbilityEndOfMove.add(:THUNDERSTORM,
+  proc { |ability, user, _targets, move, battle, _switchedBattlers|
+      next if battle.futureSight
+      next unless battle.sandy?
+      next if user.effectActive?(:Thunderstorm)
+      next if user.effectActive?(:Charge)
+      battle.pbShowAbilitySplash(user, ability)
+      user.applyEffect(:Charge)
+      user.applyEffect(:Thunderstorm)
+      battle.pbHideAbilitySplash(user)
   }
 )
 
-BattleHandlers::SpecialAttackCalcUserAbility.add(:BANSHEESMELISMA,
-  proc { |ability, _user, _battle, spAtkMult|
-      spAtkMult *= 1.5
-      next spAtkMult
+BattleHandlers::MoveImmunityTargetAbility.add(:IMPENETRABLESHELL,
+  proc { |ability, user, target, move, type, battle, showMessages, aiCheck|
+      next pbBattleMoveImmunityStatAbility(ability, user, target, move, type, :BUG, :SPEED, 1, battle, showMessages, aiCheck)
   }
 )
 
-BattleHandlers::AbilityOnSwitchIn.add(:ORICHALCHUMPRESENCE,
-  proc { |ability, battler, battle, aiCheck|
-      pbBattleWeatherAbility(ability, :DarkenedSun, battler, battle, true, true, aiCheck)
+#================================================================================================
+# Takes 15% more damage, loses 1/10th of its HP each turn but can hold two items (Well-Equippeed)
+#================================================================================================
+
+BattleHandlers::DamageCalcTargetAbility.add(:WELLEQUIPPED,
+  proc { |ability, user, target, _move, mults, _baseDmg, type, aiCheck|
+      mults[:final_damage_multiplier] *= 1.15
+      target.aiLearnsAbility(ability) unless aiCheck
   }
 )
+
+BattleHandlers::EOREffectAbility.add(:WELLEQUIPPED,
+  proc { |ability, battler, battle|
+      battle.pbShowAbilitySplash(battler, ability)
+      battler.applyFractionalDamage(EOR_SELF_HARM_ABILITY_DAMAGE_FRACTION)
+      battle.pbHideAbilitySplash(battler)
+  }
+)
+
+#================================================================================================
+# On switch-in, +1 to attacking stats in eclipse, or -2 to the enemy side in sun (Protoinstinct)
+#================================================================================================
 
 BattleHandlers::AbilityOnSwitchIn.add(:PROTOINSTINCT,
   proc { |ability, battler, battle, aiCheck|
@@ -113,12 +190,10 @@ BattleHandlers::AbilityOnSwitchIn.add(:PROTOINSTINCT,
     }
 )
 
-BattleHandlers::AbilityOnSwitchIn.add(:HADRONSYSTEM,
-  proc { |ability, battler, battle, aiCheck|
-      pbBattleWeatherAbility(ability, :BrilliantRain, battler, battle, true, true, aiCheck)
-  }
-)
-
+#==================================================================================================
+# On switch-in, +1 to attacking stats in rain, or -2 to the enemy side in moonglow (Quark Protocol)
+#===================================================================================================
+ 
 BattleHandlers::AbilityOnSwitchIn.add(:QUARKPROTOCOL,
   proc { |ability, battler, battle, aiCheck|
       if battle.rainy?
@@ -136,21 +211,22 @@ BattleHandlers::AbilityOnSwitchIn.add(:QUARKPROTOCOL,
     }
 )
 
-BattleHandlers::AddedEffectChanceModifierUserAbility.add(:PRIMALECHO,
-    proc { |ability, user, target, move, chance|
-        chance += 50 if move.soundMove?
-        next chance
-    }
-)
+#========================================================================================
+# Sets Darkened Sun in switch-in, a combination of Eclipse and Sun (Orichalchum Presence)
+#========================================================================================
 
-BattleHandlers::MoveImmunityTargetAbility.add(:IMPENETRABLESHELL,
-  proc { |ability, user, target, move, type, battle, showMessages, aiCheck|
-      next pbBattleMoveImmunityStatAbility(ability, user, target, move, type, :BUG, :SPEED, 1, battle, showMessages, aiCheck)
+BattleHandlers::AbilityOnSwitchIn.add(:ORICHALCHUMPRESENCE,
+  proc { |ability, battler, battle, aiCheck|
+      pbBattleWeatherAbility(ability, :DarkenedSun, battler, battle, true, true, aiCheck)
   }
 )
 
-BattleHandlers::StatusImmunityAbility.add(:FIGHTINGVIGOR,
-  proc { |ability, _battler, status|
-      next true if status == :LEECHED
+#=====================================================================================
+# Sets Brilliant Rain in switch-in, a combination of Rain and Moonglow (Hadron System)
+#=====================================================================================
+
+BattleHandlers::AbilityOnSwitchIn.add(:HADRONSYSTEM,
+  proc { |ability, battler, battle, aiCheck|
+      pbBattleWeatherAbility(ability, :BrilliantRain, battler, battle, true, true, aiCheck)
   }
 )
--- Plugins/ModTectonic Paradox Pokémon/Paradox Moves.rb
+++ Plugins/ModTectonic Paradox Pokémon/Paradox Moves.rb
@@ -1,129 +1,4 @@
 #===============================================================================
-# Boosts by 50% if there is sun. (Hydro Steam)
-#===============================================================================
-class PokeBattle_Move_BoostInSun < PokeBattle_Move
-    def pbBaseDamage(baseDmg, user, target)
-        baseDmg *= 1.5 if battle.sunny?
-        return baseDmg
-    end
-end
-
-#===============================================================================
-# Boosts by 50% if there is moonglow. (Psyblade)
-#===============================================================================
-class PokeBattle_Move_BoostInMoonglow < PokeBattle_Move
-    def pbBaseDamage(baseDmg, user, target)
-        baseDmg *= 1.5 if battle.moonGlowing?
-        return baseDmg
-    end
-end
-
-#===============================================================================
-# User is protected against moves with the "B" flag this round. If a Pokémon
-# attacks with the user with a physical attack while this effect applies, that Pokémon is
-# burned. (Burning Balwark)
-#===============================================================================
-class PokeBattle_Move_ProtectUserBurnPhysAttacker < PokeBattle_ProtectMove
-    def initialize(battle, move)
-        super
-        @effect = :BurningBalwark
-    end
-
-    def getEffectScore(user, target)
-        score = super
-        # Check only physical attackers
-        user.eachPredictedProtectHitter(0) do |b|
-            score += getBurnEffectScore(user, b)
-        end
-        return score
-    end
-end
-
-#===============================================================================
-# Boosts by 33% if the move is super effective. (Collision Course and Electro Drift)
-#===============================================================================
-class PokeBattle_Move_BoostIfSuperEffective < PokeBattle_Move
-    def pbBaseDamage(baseDmg, user, target)
-        baseDmg *= 1.33 if Effectiveness.super_effective?(typeModToCheck(user.battle, type, user, target, move, aiCheck))
-        return baseDmg
-    end
-end
-
-#===============================================================================
-# Two turn attack. Skips first turn, attacks second turn. In sunshine, takes 1 turn instead. (Solar Beam)
-#===============================================================================
-class PokeBattle_Move_TwoTurnAttackCanChooseOne < PokeBattle_TwoTurnMove
-
-    def resolutionChoice(user)
-        @choices = [_INTL("Charge"),_INTL("Attack")]
-        @choice = @battle.scene.pbShowCommands(_INTL("Should #{user.pbThis(true)} charge the attack?"),@choices,0)
-    end 
-
-    def pbChargingTurnMessage(user, _targets)        
-        @battle.pbDisplay(_INTL("{1} charges power!",user.pbThis))
-    end
-
-    
-    def skipChargingTurn?(user)
-        return @choice == 1
-    end
-
-    def pbBaseDamage(baseDmg, _user, target)
-        baseDmg *= 0.5 if @choice == 1
-        return baseDmg
-    end
-end
-
-
-    def resetMoveUsageState
-        @choice = nil
-    end 
-
-    def getEffectScore(_user, _target)
-        return 100
-    end 
-
-
-#===============================================================================
-# Clears hazards, weather and rooms. (Mighty Cleave?)
-#===============================================================================
-class PokeBattle_Move_RemovesHazardsWeatherRooms < PokeBattle_Move
-    def hazardRemovalMove?; return true; end
-    def aiAutoKnows?(pokemon); return false; end
-
-    def eachHazard(side, isOurSide)
-        side.eachEffect(true) do |effect, _value, data|
-            next unless data.is_hazard?
-            yield effect, data
-        end
-    end
-
-    def removeEffect(user, side, effect, data)
-        side.disableEffect(effect)
-        if data.is_hazard?
-            hazardName = data.name
-            @battle.pbDisplay(_INTL("{1} destroyed {2}!", user.pbThis, hazardName)) unless data.has_expire_proc?
-        end
-    end
-
-    def pbEffectGeneral(user)
-        targetSide = user.pbOpposingSide
-        ourSide = user.pbOwnSide
-        eachHazard(targetSide, false) do |effect, data|
-            removeEffect(user, targetSide, effect, data)
-        end
-        eachHazard(ourSide, true) do |effect, data|
-            removeEffect(user, ourSide, effect, data)
-        end
-        @battle.field.eachEffect(true) do |effect, _value, effectData|
-            next unless effectData.is_room?
-            @battle.field.disableEffect(effect)
-        end
-        @battle.endWeather
-    end
-end
-
-#===============================================================================
 # User heals for 1/4ths of their HP and removes hazards from its field. (One With The Earth) (not functional)
 #===============================================================================
 class PokeBattle_Move_HealUserOneQuartersRemoveHazards < PokeBattle_HealingMove
@@ -211,3 +86,127 @@ GameData::BattleEffect.register_effect(:Battler, {
     :resets_on_move_start => true,
     :snowballing_move_counter => true,
 })
+
+#===============================================================================
+# Two turn attack. Can be used in one turn with half its power (Atomic Breath)
+#===============================================================================
+class PokeBattle_Move_TwoTurnAttackCanChooseOne < PokeBattle_TwoTurnMove
+
+    def resolutionChoice(user)
+        @choices = [_INTL("Charge"),_INTL("Attack")]
+        @choice = @battle.scene.pbShowCommands(_INTL("Should #{user.pbThis(true)} charge the attack?"),@choices,0)
+    end 
+
+    def pbChargingTurnMessage(user, _targets)        
+        @battle.pbDisplay(_INTL("{1} charges power!",user.pbThis))
+    end
+
+    
+    def skipChargingTurn?(user)
+        return @choice == 1
+    end
+
+    def pbBaseDamage(baseDmg, _user, target)
+        baseDmg *= 0.5 if @choice == 1
+        return baseDmg
+    end
+end
+
+
+    def resetMoveUsageState
+        @choice = nil
+    end 
+
+    def getEffectScore(_user, _target)
+        return 100
+    end 
+
+#===============================================================================
+# Boosts by 50% if there is sun. (Hydro Steam)
+#===============================================================================
+class PokeBattle_Move_BoostInSun < PokeBattle_Move
+    def pbBaseDamage(baseDmg, user, target)
+        baseDmg *= 1.5 if battle.sunny?
+        return baseDmg
+    end
+end
+
+#===============================================================================
+# Boosts by 50% if there is moonglow. (Psyblade)
+#===============================================================================
+class PokeBattle_Move_BoostInMoonglow < PokeBattle_Move
+    def pbBaseDamage(baseDmg, user, target)
+        baseDmg *= 1.5 if battle.moonGlowing?
+        return baseDmg
+    end
+end
+
+#===============================================================================
+# User is protected against moves with the "B" flag this round. If a Pokémon
+# attacks with the user with a physical attack while this effect applies, that Pokémon is
+# burned. (Burning Balwark)
+#===============================================================================
+class PokeBattle_Move_ProtectUserBurnPhysAttacker < PokeBattle_ProtectMove
+    def initialize(battle, move)
+        super
+        @effect = :BurningBalwark
+    end
+
+    def getEffectScore(user, target)
+        score = super
+        # Check only physical attackers
+        user.eachPredictedProtectHitter(0) do |b|
+            score += getBurnEffectScore(user, b)
+        end
+        return score
+    end
+end
+
+#===============================================================================
+# Clears hazards, weather and rooms. (Mighty Cleave?)
+#===============================================================================
+class PokeBattle_Move_RemovesHazardsWeatherRooms < PokeBattle_Move
+    def hazardRemovalMove?; return true; end
+    def aiAutoKnows?(pokemon); return false; end
+
+    def eachHazard(side, isOurSide)
+        side.eachEffect(true) do |effect, _value, data|
+            next unless data.is_hazard?
+            yield effect, data
+        end
+    end
+
+    def removeEffect(user, side, effect, data)
+        side.disableEffect(effect)
+        if data.is_hazard?
+            hazardName = data.name
+            @battle.pbDisplay(_INTL("{1} destroyed {2}!", user.pbThis, hazardName)) unless data.has_expire_proc?
+        end
+    end
+
+    def pbEffectGeneral(user)
+        targetSide = user.pbOpposingSide
+        ourSide = user.pbOwnSide
+        eachHazard(targetSide, false) do |effect, data|
+            removeEffect(user, targetSide, effect, data)
+        end
+        eachHazard(ourSide, true) do |effect, data|
+            removeEffect(user, ourSide, effect, data)
+        end
+        @battle.field.eachEffect(true) do |effect, _value, effectData|
+            next unless effectData.is_room?
+            @battle.field.disableEffect(effect)
+        end
+        @battle.endWeather
+    end
+end
+
+#===============================================================================
+# Boosts by 33% if the move is super effective. (Collision Course and Electro Drift)
+#===============================================================================
+class PokeBattle_Move_BoostIfSuperEffective < PokeBattle_Move
+    def pbBaseDamage(baseDmg, user, target)
+        baseDmg *= 1.33 if Effectiveness.super_effective?(typeModToCheck(user.battle, type, user, target, move, aiCheck))
+        return baseDmg
+    end
+end
