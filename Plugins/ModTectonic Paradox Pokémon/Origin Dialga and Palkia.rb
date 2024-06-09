6c17d752415f70b2550d62861d4d60be2f386e5f
Valrex5 <138341257+Valrex5@users.noreply.github.com>
2024-06-09 14:43:01 -0300

    First draft of Dialga-Palkia avatars

--- Plugins/ModTectonic Paradox Pokémon/Origin Dialga and Palkia.rb
+++ Plugins/ModTectonic Paradox Pokémon/Origin Dialga and Palkia.rb
@@ -17,3 +17,47 @@ MultipleForms.register(:PALKIA,{
     next 0
   }
 })
+
+#===============================================================================
+# Starts darkened sun weather. (Noachian Light)
+#===============================================================================
+class PokeBattle_Move_StartDarkenedSun8 < PokeBattle_WeatherMove
+  include EmpoweredMove
+  def pbMoveFailed?(user, targets, show_message)
+    return false if GameData::Type.exists?(:FIRE) && !user.pbHasType?(:FIRE) && user.canChangeType?
+    super
+  end
+
+  def initialize(battle, move)
+      super
+      @weatherType = :DarkenedSun
+  end
+
+  def pbEffectGeneral(user)
+    super
+    transformType(user, :PSYCHIC)
+    user.applyEffect(:Type3, :FIRE)
+  end
+end
+
+#===============================================================================
+# Starts brilliant rain weather. (Nouvelle Night)
+#===============================================================================
+class PokeBattle_Move_StartBrilliantRain8 < PokeBattle_WeatherMove
+  include EmpoweredMove
+  def pbMoveFailed?(user, targets, show_message)
+    return false if GameData::Type.exists?(:FAIRY) && !user.pbHasType?(:FAIRY) && user.canChangeType?
+    super
+  end
+
+  def initialize(battle, move)
+      super
+      @weatherType = :BrilliantRain
+  end
+
+  def pbEffectGeneral(user)
+    super
+    transformType(user, :WATER)
+    user.applyEffect(:Type3, :FAIRY)
+  end
+end
