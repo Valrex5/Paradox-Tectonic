300986344de3d6e89601f134e02027db94af2085
Valrex5 <138341257+Valrex5@users.noreply.github.com>
2024-06-11 12:52:51 -0300

    Update BattlerEffects.rb

--- Plugins/Tectonic Battle/Effects/EffectDefinitions/BattlerEffects.rb
+++ Plugins/Tectonic Battle/Effects/EffectDefinitions/BattlerEffects.rb
@@ -2047,19 +2047,3 @@ GameData::BattleEffect.register_effect(:Battler, {
     :resets_eor => true,
 
 })
-
-GameData::BattleEffect.register_effect(:Battler, {
-    :id => :Thunderstorm,
-    :real_name => "Thunderstorm",
-})
-
-GameData::BattleEffect.register_effect(:Battler, {
-    :id => :BurningBalwark,
-    :real_name => "Burning Balwark",
-    :resets_eor	=> true,
-    :protection_info => {
-        :hit_proc => proc do |user, target, move, _battle|
-            user.applyBurn(target) if move.physicalMove? && user.canBurn?(target, false)
-        end,
-    },
-})
