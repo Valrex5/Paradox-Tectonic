5157610ba2bf3ae41c5f019b3995c335ec25900e
Valrex5 <138341257+Valrex5@users.noreply.github.com>
2025-08-13 17:15:07 -0300

    different changes

--- Plugins/Mod Tectonic Goku/ability.rb
+++ Plugins/Mod Tectonic Goku/ability.rb
@@ -1,25 +1,80 @@
+#BattleHandlers::AbilityOnSwitchIn.add(:SAIYANWARRIOR,
+#  proc { |ability, battler, battle, aiCheck|
+#      trigger = battler.pbDirectOpposing 
+#      next unless battler.countsAs?(:GOKU)
+#      next unless battler.form == 0
+#      next unless trigger.countsAs?(:REGIGIGAS)
+#      next unless trigger.boss?
+#      battler.pbChangeForm(6, _INTL("{1} calms down in the face of the enemy.", battler.pbThis))
+#  }
+#)
+
+MultipleForms.register(:GOKU, {
+  "getFormOnLeavingBattle" => proc { |pkmn, _battle, _usedInBattle, endBattle|
+      next 0 if pkmn.fainted? || endBattle
+  },
+})
+
 BattleHandlers::AbilityOnSwitchIn.add(:SAIYANWARRIOR,
-  proc { |ability, battler, battle, aiCheck|
-      trigger = battler.pbDirectOpposing 
-      next unless battler.countsAs?(:GOKU)
-      next unless battler.form == 0
-      next unless trigger.countsAs?(:REGIGIGAS)
-      next unless trigger.boss?
-      battler.pbChangeForm(6, _INTL("{1} calms down in the face of the enemy.", battler.pbThis))
+  proc { |ability, battler, battle, aiCheck, scene|
+    next unless battler.species == :GOKU
+    next unless battler.form == 0
+    next if battler.level < 25
+    form0Name = GameData::Species.get_species_form(:GOKU,0).form_name
+    form1Name = GameData::Species.get_species_form(:GOKU,1).form_name
+    form2Name = GameData::Species.get_species_form(:GOKU,2).form_name
+    form3Name = GameData::Species.get_species_form(:GOKU,4).form_name
+    if battler.level >= 25 && battler.level <= 39
+      choices = [form0Name,form1Name]
+    elsif battler.level >= 40 && battler.level <= 59
+      choices = [form0Name,form1Name,form2Name]
+    elsif battler.level >= 60
+      choices = [form0Name,form1Name,form2Name,form3Name]
+    end 
+    if battle.autoTesting
+    choice = rand(1)
+    elsif !battler.pbOwnedByPlayer? # Trainer AI
+    choice = 0
+    else
+    choice = battle.scene.pbShowCommands(_INTL("What form should it take?"),choices,0)
+    end 
+    if choice == 3
+      @chosenForm = choice + 1
+    else
+      @chosenForm = choice
+    end 
+    battler.pbChangeForm(@chosenForm, _INTL("{1} transforms!", battler.pbThis))
   }
+)
+
+
+BattleHandlers::AbilityChangeOnBattlerFainting.add(:SAIYANWARRIOR,
+    proc { |ability, battler, fainted, battle|
+        next if battler.opposes?(fainted)
+        next unless battler.species == :GOKU
+        next unless battler.form == 0
+        next unless battler.level >= 15
+        battler.pbChangeForm(1, _INTL("{1} is filled with rage!", battler.pbThis))
+    }
 )
 
 BattleHandlers::FullMoonAbility.add(:SAIYANWARRIOR,
     proc { |ability, battler, battle|
         next unless battler.species == :GOKU
-        next unless battler.form == 0
-        battler.pbChangeForm(8, _INTL("{1}'s transforms with exposure to the Full Moon!", battler.pbThis))
+        if battler.form == 0 && battler.level >= 20
+          battler.pbChangeForm(8, _INTL("{1}'s transforms with exposure to the Full Moon!", battler.pbThis))
+        elsif battler.form == 1 && battler.level >= 40
+          battler.pbChangeForm(9, _INTL("{1}'s transforms with exposure to the Full Moon!", battler.pbThis))
+        end 
     }
 )
 
-BattleHandlers::AbilityOnSwitchIn.add(:SAIYANWARRIOR,
+BattleHandlers::EOREffectAbility.add(:GREATAPE,
   proc { |ability, battler, battle, aiCheck|
     next 0 if aiCheck
+    next unless battler.species == :GOKU
+    next unless battler.form == 9
+    next if battler.effectActive?(:GoldenOozaru)    
     battler.showMyAbilitySplash(ability)
     battler.applyEffect(:GoldenOozaru)
     battler.hideMyAbilitySplash
@@ -32,14 +87,22 @@ GameData::BattleEffect.register_effect(:Battler, {
     :trapping => true,
     :apply_proc => proc do |battle, battler, _value|
         battle.pbDisplay(_INTL("{1} won't leave the battle!", battler.pbThis))
-    end
+    end,
+    :eor_proc => proc do |battle, battler, _value|
+        if battler.takesIndirectDamage?
+            battle.pbDisplay(_INTL("{1} damages himself in his rampage!", battler.pbThis))
+            battler.applyFractionalDamage(APE_DAMAGE_FRACTION, false)
+        end 
+    end, 
 })
 
+APE_DAMAGE_FRACTION = 0.20
 
-BattleHandlers::UserAbilityEndOfMove.add(:SAIYANWARRIOR,
+BattleHandlers::UserAbilityEndOfMove.add(:GREATAPE,
   proc { |ability, user, targets, move, battle, _switchedBattlers|
       next unless user.species == :GOKU
       next unless user.form == 9
+      next unless user.level >= 50
       next if battle.pbAllFainted?(user.idxOpposingSide)
       numFainted = 0
       targets.each { |b| numFainted += 1 if b.damageState.fainted }
@@ -50,3 +113,4 @@ BattleHandlers::UserAbilityEndOfMove.add(:SAIYANWARRIOR,
       battle.pbHideAbilitySplash(user)
   }
 )
+
--- Plugins/Mod Tectonic Goku/super saiyan.rb
+++ Plugins/Mod Tectonic Goku/super saiyan.rb
@@ -1,10 +1,13 @@
-class PokeBattle_Move_ChangeUserGokuForm < PokeBattle_TwoTurnMove
+class PokeBattle_Move_ChangeUserGokuForm < PokeBattle_Move
   def pbMoveFailed?(user, _targets, show_message)
       if !user.countsAs?(:GOKU)
           @battle.pbDisplay(_INTL("But {1} can't use the move!", user.pbThis(true))) if show_message
           return true
-      elsif not (user.form == 0 || user.form == 8)
+      elsif user.form == 3 || user.form == 5 || user.form == 6 || user.form == 7 || user.form == 9 || user.form == 10
           @battle.pbDisplay(_INTL("But {1} can't use it the way it is now!", user.pbThis(true))) if show_message
+          return true
+      elsif user.form == 0 && user.level <= 19 || user.form == 1 && user.level <= 30
+          @battle.pbDisplay(_INTL("But {1} can't use it at this level!", user.pbThis(true))) if show_message
           return true
       end
       return false
@@ -12,7 +15,15 @@ class PokeBattle_Move_ChangeUserGokuForm < PokeBattle_TwoTurnMove
 
 
   def pbEffectGeneral(user)
-    if user.form == 8
+    if user.form == 0
+      user.pbChangeForm(1, _INTL("{1} transformed!", user.pbThis))
+    elsif user.form == 1
+      user.pbChangeForm(2, _INTL("{1} transformed!", user.pbThis))
+    elsif user.form == 2
+      user.pbChangeForm(3, _INTL("{1} transformed!", user.pbThis))
+    elsif user.form == 4
+        user.pbChangeForm(5, _INTL("{1} transformed!", user.pbThis))
+    elsif user.form == 8
       user.pbChangeForm(9, _INTL("{1} transformed!", user.pbThis))
     end 
   end
@@ -22,5 +33,9 @@ class PokeBattle_Move_ChangeUserGokuForm < PokeBattle_TwoTurnMove
       score += 100
       score += 50 if user.firstTurn?
       return score
+  end
+
+  def resetMoveUsageState
+    @chosenForm = nil
   end
 end 
